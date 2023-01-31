76136bfb95bb96c687af62cad17078f1550a44d9
Vlad <zitarvlad11@gmail.com>
2023-01-31 17:24:48 +0600

    Recipe Book

--- src/main/java/com/example/homeworkforurok3_4webapplicationstructure/model/Recipe.java
+++ src/main/java/com/example/homeworkforurok3_4webapplicationstructure/model/Recipe.java
@@ -1,4 +1,5 @@
 package com.example.homeworkforurok3_4webapplicationstructure.model;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,11 +8,11 @@ import java.util.List;
 
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Recipe {
     private String recipeName;
     private int cookingTime;
     List<String> ingredient = new ArrayList<>();
     List<String> cookingSteps = new ArrayList<>();
-
     }
